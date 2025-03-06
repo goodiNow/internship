c3267089f0d406849ec8f33ada99f6a6a7e5da5d
goodinow <gooodinow@gmail.com>
2025-03-06 17:44:42 +0500

    commit3

--- Module_2/2.10/script.js
+++ Module_2/2.10/script.js
@@ -1,4 +1,6 @@
-let number = prompt("Введите число");
+//task 1
+
+let number = prompt('Введите число');
 if (number > 0) {
     alert("1")
 } else if (number === 0) {
@@ -6,3 +8,17 @@ if (number > 0) {
 } else {
     alert("-1")
 }
+
+//task 2
+
+let result;
+
+let check1 = (a + b > 4) ? result = 'Мало' : result = 'Много';
+
+//task 3
+
+let message;
+
+let check2 = (login == 'Сотрудник') ? message = 'Привет' :
+(login == 'Директор') ? message = 'Здраствуйте' :
+(login == '') ? message = 'Нет логина' : message = '';
